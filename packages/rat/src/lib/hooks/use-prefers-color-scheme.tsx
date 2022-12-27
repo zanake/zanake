f9f8170ac63d67c469114f4b567f03899250e11e
Mnengwa <howardmnengwa@gmail.com>
2022-12-27 07:48:15 +0300

    fix: handle window is not defined exception in usePrefersColorScheme hook

--- packages/rat/src/lib/hooks/use-prefers-color-scheme.tsx
+++ packages/rat/src/lib/hooks/use-prefers-color-scheme.tsx
@@ -3,24 +3,41 @@ import { useState, useEffect } from "react";
 /**
  * Checks if the host machine has the light theme activated
  * 
+ * @param {'dark'|'light'} fallback - default theme in case system default is not detected
+ * 
  * @returns {boolean}
  */
-const usePrefersColorScheme = () : boolean => {
-    const currentColorScheme = () => window.matchMedia("(prefers-color-scheme: light)").matches;
+const usePrefersColorScheme = (fallback: 'dark'|'light') : boolean => {
+    const currentColorScheme = () => {
+        let lightThemed = fallback === 'light';
+
+        try {
+            if (typeof window !== "undefined") lightThemed = window.matchMedia("(prefers-color-scheme: light)").matches;
+        } catch (error) {
+            console.debug(error);
+        }
+
+        return lightThemed;
+    }
 
     const [lightThemed, setLightThemed] = useState(currentColorScheme());
 
     const mediaQueryListener = ((event: MediaQueryListEvent) => setLightThemed(event.matches));
 
     useEffect(() => {
-        if (typeof window !== undefined) {
-            const lightThemeMediaQuery = window.matchMedia("(prefers-color-scheme: light)");
-            lightThemeMediaQuery.addEventListener("change", mediaQueryListener);
+        try {
+            if (typeof window !== "undefined") {
+                const lightThemeMediaQuery = window.matchMedia("(prefers-color-scheme: light)");
+                lightThemeMediaQuery.addEventListener("change", mediaQueryListener);
 
-            return () => lightThemeMediaQuery.removeEventListener("change", mediaQueryListener);
+                return () => lightThemeMediaQuery.removeEventListener("change", mediaQueryListener);
+            }
+
+            return;
+        } catch (error) {
+            console.debug(error);
+            return;
         }
-
-        return;
     }, []);
 
     return lightThemed;
