1f8ad0339addd9ccf1319282361dba196b3b9b82
Howard Mnengwa <25894745+mnengwa@users.noreply.github.com>
2023-01-02 20:27:57 +0300

    fix: initially pick up the system theme before "change" event listener is triggered in usePrefersColorScheme() hook

--- packages/rat/src/lib/hooks/use-prefers-color-scheme.tsx
+++ packages/rat/src/lib/hooks/use-prefers-color-scheme.tsx
@@ -4,6 +4,7 @@ export type theming = "dark" | "light";
 
 /**
  * Returns the host machine (i.e iPhone, Andriod, windows) preferred color scheme
+ * Default to fallback then "light", in case of any error
  * 
  * @param {theming} fallback - default color scheme in case system default is not detected, if none is given it defaults to "light"
  * 
@@ -12,21 +13,14 @@ export type theming = "dark" | "light";
 const usePrefersColorScheme = (fallback: theming) : theming => {
     const [state, setState] = useState(fallback || "light");
 
-    const listener = ((event: MediaQueryListEvent) => setState(event.matches ? "light" : "dark"));
-
     useEffect(() => {
-        try {
-            if (typeof window !== "undefined") {
-                const query = window.matchMedia("(prefers-color-scheme: light)");
-                query.addEventListener("change", listener);
+        if (typeof window !== undefined) {
+            const listener = ((event: MediaQueryListEvent) => setState(event.matches ? "light" : "dark"));
+            const query = window.matchMedia("(prefers-color-scheme: light)");
+            setState(query.matches ? "light" : "dark");
+            query.addEventListener("change", listener);
 
-                return () => query.removeEventListener("change", listener);
-            }
-
-            return;
-        } catch (error) {
-            console.error(error);
-            return;
+            return () => query.removeEventListener("change", listener);
         }
     }, []);
 
