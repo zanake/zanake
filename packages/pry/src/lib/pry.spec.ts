a7c21419e4b59693ade3e6593e7c07673b5a8089
mnengwa <howard@mnengwa.com>
2023-08-30 21:25:35 +0300

    fix: tslib dependency

--- packages/pry/src/lib/pry.spec.ts
+++ packages/pry/src/lib/pry.spec.ts
@@ -1,27 +1,28 @@
-import path from "path";
-import fs from "fs/promises";
-import pry, { Pry } from './pry';
+import path from 'path';
+import fs from 'fs/promises';
+import type { Logger } from './pry';
+import { logger as pry } from './pry';
 
 const logs = [
-    {level: "log", title: "Hi", message: "Hello, World"},
-    {level: "info", title: "Reporting", message: "Status 200 OK"},
-    {level: "warn", title: "Ignored issue", message: "Houston, we've had a problem here."},
-    {level: "error", title: "App failure", message: "Mayday! Mayday! We are going down."},
-    {level: "debug", title: "Blocker bug", message: "Not what's happening here."}
+    { level: 'log', title: 'Hi', message: 'Hello, World' },
+    { level: 'info', title: 'Reporting', message: 'Status 200 OK' },
+    { level: 'warn', title: 'Ignored issue', message: "Houston, we've had a problem here." },
+    { level: 'error', title: 'App failure', message: 'Mayday! Mayday! We are going down.' },
+    { level: 'debug', title: 'Blocker bug', message: "Not what's happening here." },
 ];
 
-const mkdir = async(directory: string)  => await fs.mkdir(path.resolve(process.cwd(), directory), {recursive:true});
-const rimraf = async(directory: string) => await fs.rm(path.resolve(process.cwd(), directory), {recursive: true, force: true});
+const mkdir = async (directory: string) => await fs.mkdir(path.resolve(process.cwd(), directory), { recursive: true });
+const rimraf = async (directory: string) =>
+    await fs.rm(path.resolve(process.cwd(), directory), { recursive: true, force: true });
 
-
-describe('pry', () => {
+describe('file logger', () => {
     describe('writes to standard error / output', () => {
         logs.forEach((log) => {
             const name = Object.values(log).join(' - ');
-            test(name, async () => await pry(log as Pry));
+            test(name, async () => await pry(log as Logger));
         });
     });
-    
+
     describe('writes to file given an absolute path', () => {
         const directory = path.join(process.cwd(), 'tmp', 'pry', 'logs');
 
@@ -37,9 +38,9 @@ describe('pry', () => {
 
         logs.forEach((log) => {
             test(Object.values(log).join(' - '), async () => {
-                const logger = async (config: Pry) => await pry({...config, directory});
+                const logger = async (config: Logger) => await pry({ ...config, directory });
 
-                await logger(log as Pry);
+                await logger(log as Logger);
             });
         });
     });
@@ -59,9 +60,9 @@ describe('pry', () => {
 
         logs.forEach((log) => {
             test(Object.values(log).join(' - '), async () => {
-                const logger = async (config: Pry) => await pry({...config, directory});
+                const logger = async (config: Logger) => await pry({ ...config, directory });
 
-                await logger(log as Pry);
+                await logger(log as Logger);
             });
         });
     });
